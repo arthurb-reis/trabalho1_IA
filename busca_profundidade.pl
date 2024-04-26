92bfb2ef79b1e6f0d0924aad54be4be56c13116c
Arthur Reis <99774782+arthurb-reis@users.noreply.github.com>
2024-04-26 10:45:44 -0300

    Update busca_profundidade.pl

--- busca_profundidade.pl
+++ busca_profundidade.pl
@@ -9,9 +9,15 @@ disponibilidade_professores([
     [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1]
 ]).
 
+% Recebe o nro do horário e retorna o nro da disciplina disponível nesse horário
 disponivel(Nro, Disciplina) :-
+    % Atribui a matriz a Disponibilidades
     disponibilidade_professores(Disponibilidades),
+    % nth0(?Index, ?List, ?Elem)
+    % Varre a matriz Disponibilidades e associa o index da linha a variável Disciplina
+    	% e a linha atual a variável LinhaAtual
     nth0(Disciplina, Disponibilidades, LinhaAtual),
+    % Retorna o Valor do elemento de index Nro da LinhaAtual
     nth0(Nro, LinhaAtual, Valor),
     Valor =:= 1.
     
@@ -35,4 +41,4 @@ alocar([H|Resto], Nro_horario, Horarios_necessarios) :-
     NovoNroHorario is Nro_horario + 1,
     alocar(Resto, NovoNroHorario, Novos_horarios_necessarios).
 
-%Exemplo de teste: ?- alocar(Lista, 0, [4,4,3,3,3,3,3,2,2]).
+%Exemplo de teste: ?- alocar(Lista, 0, [4,4,3,3,3,3,3,2]).
